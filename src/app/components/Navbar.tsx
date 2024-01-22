fdbfed7606fd6be9bff10300c34c732b7138f34f
abirpahlwan <abir978@gmail.com>
2024-01-22 10:51:41 +0600

    tailwind apply css

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar() {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Hootum</span>
                 </a>
                 <div className="hidden w-full md:block md:w-auto">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li><Link href="/server"           className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Server</Link></li>
-                        <li><Link href="/client"           className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Client</Link></li>
-                        <li><Link href="/extra"            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Extra</Link></li>
-                        <li><Link href="/api/auth/signin"  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign In</Link></li>
-                        <li><Link href="/api/auth/signout" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign Out</Link></li>
+                    <ul className="navlist">
+                        <li><Link href="/dashboard"        className="navlink">Dashboard</Link></li>
+                        <li><Link href="/server"           className="navlink">Server</Link></li>
+                        <li><Link href="/client"           className="navlink">Client</Link></li>
+                        <li><Link href="/api/auth/signin"  className="navlink">Sign In</Link></li>
+                        <li><Link href="/api/auth/signout" className="navlink">Sign Out</Link></li>
                     </ul>
                 </div>
             </div>
